251bf3cb04d94008e97182b004c7d9eb69fca5ea
aklimk <andrewklimko@outlook.com>
2024-11-30 15:56:49 +1000

    Split os config into seperate script.

---
+++ install/setup-user.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+choice = input("~~~~ Install vim? (y/n) ")
+if choice == "y":
+    os.system("pacman -S vim")
+
+username = input("~~~~ Choose a username ")
+os.system("useradd -m " + username)
+os.system("passwd " + username)
+
+print("~~~~ Installing sudo")
+choice = input("~~~~ Install sudo? (y/n) ")
+if choice == "y":
+    os.system("pacman -S sudo")
+    os.system("EDITOR=vim visudo")
+    os.system("exit")
